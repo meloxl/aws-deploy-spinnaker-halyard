a10267dd95e620cd472db93cc56ab1f9ce7c0403
tiny <409669370@163.com>
2018-11-21 10:43:18 +0800

    Update deploy_spinnaker.sh

--- scripts/deploy_spinnaker.sh
+++ scripts/deploy_spinnaker.sh
@@ -66,7 +66,7 @@ SPINNAKER_MANAGED_ROLE="role/SpinnakerManaged"
 echo "Creating some kubernetes resources before running halyard"
 kubectl apply -f resources/kubernetes/lb-services.yaml -n spinnaker
 kubectl apply -f resources/kubernetes/spinnaker-k8s-role.yaml
-
+whoami
 GATE_ADDRESS=$(kubectl describe svc gate-lb -n spinnaker | grep LoadBalancer\ Ingress | awk '{print $3}')
 DECK_ADDRESS=$(kubectl describe svc deck-lb -n spinnaker | grep LoadBalancer\ Ingress | awk '{print $3}')
 until [ "${GATE_ADDRESS}" != "" ]; do
