e03822a3841f9b07c60ebc275f34ab07fc4b50b3
tiny <409669370@163.com>
2018-11-21 11:27:49 +0800

    Update deploy_spinnaker.sh

--- scripts/deploy_spinnaker.sh
+++ scripts/deploy_spinnaker.sh
@@ -137,6 +137,7 @@ TOKEN=$(kubectl get secret \
    -o jsonpath='{.data.token}' | base64 -d)
 kubectl config set-credentials ${CONTEXT}-token-user --token ${TOKEN}
 kubectl config set-context ${CONTEXT}-spinnaker-context --cluster=kubernetes --user=${CONTEXT}-token-user
+kubectl config use-context ${CONTEXT}-spinnaker-context
 
 hal --color false config provider kubernetes account add my-${CONTEXT}-k8s-account --provider-version v2 --context ${CONTEXT}-spinnaker-context --namespaces default,spinnaker
 
