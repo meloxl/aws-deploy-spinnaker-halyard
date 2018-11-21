572f4494a347d6a156e875c015043b946294d8a3
tiny <409669370@163.com>
2018-11-21 11:42:03 +0800

    Update deploy_spinnaker.sh

--- scripts/deploy_spinnaker.sh
+++ scripts/deploy_spinnaker.sh
@@ -126,6 +126,7 @@ hal --color false config provider aws enable
 CONTEXT="mfdev"
 id
 ls -al resources/kubernetes/
+cat resources/kubernetes/mfdev-rw.config
 export KUBECONFIG=resources/kubernetes/mfdev-rw.config
 kubectl cluster-info
 kubectl describe namespace spinnaker && echo "Namespace already exists" || kubectl create namespace spinnaker
