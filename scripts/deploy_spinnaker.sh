20822c46adb01baf6e70680fac2c97e2322e3cfa
tiny <409669370@163.com>
2018-11-21 16:43:35 +0800

    Update deploy_spinnaker.sh

--- scripts/deploy_spinnaker.sh
+++ scripts/deploy_spinnaker.sh
@@ -185,6 +185,8 @@ fi
 
 hal --color false config deploy edit --type distributed --account-name my-k8s-account
 
+hal --color false config deploy edit --type distributed --account-name my-${CONTEXT}-k8s-account
+
 hal --color false config version edit --version 1.10.1
 
 #mkdir -p /home/spinnaker/.hal/default/service-settings
